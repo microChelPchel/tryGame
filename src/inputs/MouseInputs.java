89bfe0db35e4e84542fdb0927bdfdf4489e80110
Sokolov Alexandr <sachacok@yandex.ru>
2023-01-02 23:04:46 +0300

    create game loops

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -47,6 +47,6 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        gamePanel.setRectPos(e.getX(),e.getY());
+       // gamePanel.setRectPos(e.getX(),e.getY());
     }
 }

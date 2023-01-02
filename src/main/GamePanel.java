06b7849298d2cb8b59f851f3731e6bb3501c7288
Sokolov Alexandr <sachacok@yandex.ru>
2023-01-02 20:57:49 +0300

    added inputs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,18 +1,45 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.JPanel;
 import java.awt.Graphics;
 
 public class GamePanel extends JPanel {
 
-    public GamePanel() {
+    private MouseInputs mouseInputs;
 
+    private int xDelta = 100;
+    private int yDelta = 100;
+
+    public GamePanel() {
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawRect(100,100, 200, 50);
+        g.drawRect(xDelta, yDelta, 200, 50);
     }
 
 
